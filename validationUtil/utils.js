815e52d0c4a234d40fb421d3c2b5c977f969447b
AwaleRohin <rohinawale331@gmail.com>
2019-05-16 18:04:18 +0545

    fixed signup and added email verification

---
+++ validationUtil/utils.js
@@ -0,0 +1,10 @@
+var exports =module.exports={}
+    
+exports.validateEmail = (email)=> {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email)
+}
+exports.validatePassword = (password) =>{
+    var re =/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/
+    return re.test(password)
+}
