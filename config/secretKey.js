8174935613522e17de51815035d78dfa646a617f
AwaleRohin <rohinawale331@gmail.com>
2019-05-20 10:13:10 +0545

    removed secret keys

--- config/secretKey.js
+++
@@ -1,9 +0,0 @@
-module.exports= {
-    token : {
-        key : 'secret'
-    },
-    facebook :{
-        clientID : '507835646419191',
-        clientSecret : 'c9bebf77770a4fc37edddc9caa22387a'
-    }
-}
