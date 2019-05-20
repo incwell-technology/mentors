095b31f97832d5c1c038997026a314b72627b062
AwaleRohin <rohinawale331@gmail.com>
2019-05-20 10:08:49 +0545

    removed secretKey

--- routes/signUpRoute.js
+++ routes/signUpRoute.js
@@ -1,7 +1,12 @@
 const router = require('express').Router()
 const Register = require('../controller/signUpController')
+const passport = require('passport')
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
 
 router.route('/signup',).post(Register.create)
 router.route('/confirmation').get(Register.confirmation)
-router.route('/resendToken',).post(Register.resendToken)
+router.route('/resendToken').post(Register.resendToken)
+
 module.exports = router;
