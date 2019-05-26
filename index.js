3b3ad1a35b3ae354a30b17d6e70af42a4995dfac
Rajesh <rajesh.k.khadka@gmail.com>
2019-05-26 12:46:21 +0545

    update port

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ app.get('/', (req, res)=> {
   res.json({"hey": "Are you looking for mentors"})
 })
 
-app.listen(3000, () =>
+const PORT = process.env.PORT || 3000
+
+
+app.listen(PORT, () =>
   console.log('Hello Mentors')
 );
