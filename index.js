0caa0bb8ecccbf725d817a3a575c83f568069ebc
Rajesh <rajesh.k.khadka@gmail.com>
2019-05-26 12:40:31 +0545

    add response on default root

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (req, res)=> {
+  res.json({"hey": "Are you looking for mentors"})
+})
+
 app.listen(3000, () =>
-  console.log('Hello Mentors'),
+  console.log('Hello Mentors')
 );
