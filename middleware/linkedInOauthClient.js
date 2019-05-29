83bc368a88517879e7d9e734db01a9a50b7a2bc2
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2019-05-29 16:49:16 +0545

    linkedin oauth client directly returns email and name

--- middleware/linkedInOauthClient.js
+++ middleware/linkedInOauthClient.js
@@ -4,19 +4,26 @@ require('dotenv').config({
 const request = require('request-promise-native');
 
 const getEmail = async (bearerToken) => {
-    return await request.get('https://api.linkedin.com/v2/clientAwareMemberHandles?q=members&projection=(elements*(primary,type,handle~))', {
+    let emailResponse =  await request.get('https://api.linkedin.com/v2/clientAwareMemberHandles?q=members&projection=(elements*(primary,type,handle~))', {
         'auth': {
             'bearer': bearerToken
         }
     });
+    return JSON.parse(emailResponse).elements[0]["handle~"].emailAddress;
 };
 
 const getProfile = async (bearerToken) => {
-    return await request.get('https://api.linkedin.com/v2/me', {
+    let profileResponse = await request.get('https://api.linkedin.com/v2/me', {
         'auth': {
             'bearer': bearerToken
         }
     });
+    profileResponse = JSON.parse(profileResponse);
+    return { 
+        firstName: profileResponse.localizedFirstName,
+        lastName: profileResponse.localizedLastName,
+        linkedinId: profileResponse.id
+    };
 };
 
 module.exports = {
