3319a7ebc413bb6c0a49191b1360cb7728908439
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2019-05-29 13:41:10 +0545

    Added /linkedin route for authentication

--- routes/socialRoute.js
+++ routes/socialRoute.js
@@ -2,9 +2,11 @@ const router = require('express').Router()
 const Facebook = require('../controller/facebookContoller')
 const signupValidation = require('../middleware/signupValidation')
 const google = require('../controller/googleController')
+const linkedin = require('../controller/linkedInController')
 
 router.route('/facebook').post(Facebook.facebook)
 router.route('/reauthorize').post(signupValidation.validate('createUser'),Facebook.reauthorize)
 router.route('/google').post(google.oauthHandler);
+router.route('/linkedin').post(linkedin.oauthHandler);
 
 module.exports = router;    
