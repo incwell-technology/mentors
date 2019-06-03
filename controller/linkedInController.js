8bee54d4d67954f7387a07e843798ed7d8b4a252
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2019-06-03 14:53:28 +0545

    fix: merged with Rohin's code

--- controller/linkedInController.js
+++ controller/linkedInController.js
@@ -4,7 +4,7 @@ require('dotenv').config({
 
 const httpStatus = require('http-status-codes');
 const linkedinOAuth = require('../middleware/linkedInOauthClient');
-const dbQuery = require('../dbQuery/socialMediaQuery');
+const socialQuery = require('./socialQueryController');
 const tokenGenerator = require('./authTokenGenerator');
 const statusMsg = require('../config/statusMsg')
 
@@ -23,45 +23,53 @@ module.exports.oauthHandler = async (req, res) => {
             refresh_token,
             data: userInfo
         };
+
+
         try {
-            //If the linkedin id already exists in the db, update refresh token in the db
-            if (await dbQuery.isExistingUser("linkedin_id", userInfo.id, refresh_token)) {
-                return res.status(httpStatus.OK).json({
-                    "success": statusMsg.success.msg,
-                    "payload": payload
-                });
-            //if the linkedin account doesn't have email, send error message 
-            } else if (!userInfo.email) {
-                return res.status(http.CONFLICT).json({
-                    "success": statusMsg.fail.msg,
-                    "payload": userInfo
-                })
-            //if the linkedin email exists in the db, add linkedin id
-            } else if (await dbQuery.doesUserExistWithEmail(userInfo.email)) {
-                await dbQuery.addSocialMediaId("linkedin_id", userInfo, refresh_token);
-                return res.status(httpStatus.OK).json({
-                    "success": statusMsg.success.msg,
-                    "payload": payload
-                });
-                //otherwise, create a new account
-            } else {
-                await dbQuery.createAccount("linkedin_id", userInfo, refresh_token);
-                return res.status(httpStatus.CREATED).json({
-                    "success": statusMsg.success.msg,
-                    "payload": payload
-                });
-            }
-        } catch (error) {
-            console.log(error);
-            return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-                "success": statusMsg.fail.msg,
-                "payload": {},
-                "error": {
-                    "code": httpStatus.INTERNAL_SERVER_ERROR,
-                    "message": statusMsg.database_error.msg
-                }
-            });
+            socialQuery.socialQueryController('linkedin_id', userInfo, payload, res)
+        } catch (err) {
+            err.status = http.INTERNAL_SERVER_ERROR
+            next(err)
         }
+        // try {
+        //     //If the linkedin id already exists in the db, update refresh token in the db
+        //     if (await socialAuth.dbQuery.isExistingUser("linkedin_id", userInfo.id, refresh_token)) {
+        //         return res.status(httpStatus.OK).json({
+        //             "success": statusMsg.success.msg,
+        //             "payload": payload
+        //         });
+        //     //if the linkedin account doesn't have email, send error message 
+        //     } else if (!userInfo.email) {
+        //         return res.status(http.CONFLICT).json({
+        //             "success": statusMsg.fail.msg,
+        //             "payload": userInfo
+        //         })
+        //     //if the linkedin email exists in the db, add linkedin id
+        //     } else if (await dbQuery.doesUserExistWithEmail(userInfo.email)) {
+        //         await dbQuery.addSocialMediaId("linkedin_id", userInfo, refresh_token);
+        //         return res.status(httpStatus.OK).json({
+        //             "success": statusMsg.success.msg,
+        //             "payload": payload
+        //         });
+        //         //otherwise, create a new account
+        //     } else {
+        //         await dbQuery.createAccount("linkedin_id", userInfo, refresh_token);
+        //         return res.status(httpStatus.CREATED).json({
+        //             "success": statusMsg.success.msg,
+        //             "payload": payload
+        //         });
+        //     }
+        // } catch (error) {
+        //     console.log(error);
+        //     return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+        //         "success": statusMsg.fail.msg,
+        //         "payload": {},
+        //         "error": {
+        //             "code": httpStatus.INTERNAL_SERVER_ERROR,
+        //             "message": statusMsg.database_error.msg
+        //         }
+        //     });
+        // }
     } catch (error) {
         console.log(error);
         return res.status(httpStatus.BAD_REQUEST).json({
