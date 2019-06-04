16d9b5a7b99d61f54ce553b5126863696f3d935e
Vipul Chaudhary <vpul.chaudhary@gmail.com>
2019-06-04 16:04:02 +0545

    feat: verify jwt token

--- middleware/jwtValidation.js
+++ middleware/jwtValidation.js
@@ -1,9 +1,25 @@
+const jwt = require('jsonwebtoken');
+const httpStatus = require('http-status-codes');
+const secret = require('../config/secretKey');
+
 exports.verifyToken = (req, res, next) => {
-    const bearerHeader = req.headers['authorization'];
-    if (typeof bearerHeader !== 'undefined') {
-        const bearer = bearerHeader.split(' ');
-        const bearerToken = bearer[1];
-        req.token = bearerToken;
-        next();
+    let token = req.headers['authorization'];
+    if (token) {
+        if (token.startsWith('Bearer')) {
+            token = token.split(' ')[1];
+        }
+        jwt.verify(token, secret.token.key, (error, decoded) => {
+            if (error) {
+                error.status = httpStatus.UNAUTHORIZED;
+                next(error);
+            } else {
+                res.locals.email = decoded.email;
+                next();
+            }
+        });
+    } else {
+        error = Error("A valid bearer token must be sent in the header.");
+        error.status = httpStatus.UNAUTHORIZED;
+        next(error);
     }
 }
